c047dc7a3adc981a070866eb6964b9cb6b6b3660
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-17 13:31:45 +0530

    feat(question): question html to text

--- src/deserializers/question_content.rs
+++ src/deserializers/question_content.rs
@@ -1,3 +1,4 @@
+use html2text::from_read;
 #[derive(Debug, serde::Deserialize)]
 pub struct QueryQuestionContent {
     pub question: QuestionContent,
@@ -11,4 +12,16 @@ pub struct QuestionContent {
 #[derive(Debug, serde::Deserialize)]
 pub struct Data {
     pub data: QueryQuestionContent,
+}
+
+impl QuestionContent {
+    pub fn html_to_text(&self) -> String {
+        let string = self.content.as_bytes();
+        let s: String = from_read(string, string.len());
+        s.replace("\\n\\n", "\n\n")
+            .lines()
+            .filter(|l| *l != "")
+            .collect::<Vec<&str>>()
+            .join("\n")
+    }
 }
