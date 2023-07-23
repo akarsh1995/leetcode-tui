ce5a939062a4ce5e6fbd071b6c8d6e83b9931566
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-23 16:39:09 +0530

    feat: run and submit code requests

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -3,11 +3,11 @@ use serde::{de::DeserializeOwned, Serialize};
 use serde_json::{json, Value};
 pub mod problemset_question_list;
 pub mod question_content;
+pub mod run_code;
+pub mod submit;
 use crate::errors::AppResult;
 
 pub type QuestionContentQuery = question_content::Query;
-
-const LEETCODE_GRAPHQL_ENDPOINT: &str = "https://leetcode.com/graphql";
 
 #[async_trait]
 pub trait GQLLeetcodeQuery: Serialize {
@@ -17,18 +17,212 @@ pub trait GQLLeetcodeQuery: Serialize {
         json!(self)
     }
 
-    fn get_endpoint(&self) -> &'static str {
-        LEETCODE_GRAPHQL_ENDPOINT
+    fn is_post(&self) -> bool {
+        true
+    }
+
+    fn get_endpoint(&self) -> String {
+        "https://leetcode.com/graphql".to_string()
     }
 
     async fn post(&self, client: &reqwest::Client) -> AppResult<Self::T> {
-        Ok(client
-            .post(self.get_endpoint())
+        let request;
+        if self.is_post() {
+            request = client.post(self.get_endpoint()).json(&self.get_body());
+        } else {
+            request = client.get(self.get_endpoint());
+        }
+        Ok(request
             .header("Content-Type", "application/json")
-            .json(&self.get_body())
             .send()
             .await?
             .json()
             .await?)
+    }
+}
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct LanguageInfo {
+    id: u32,
+    name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Data {
+    language_list: Vec<LanguageInfo>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Languages {
+    data: Data,
+}
+
+// Generate the enum for languages
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Language {
+    Cpp,
+    Java,
+    Python,
+    Python3,
+    Mysql,
+    Mssql,
+    Oraclesql,
+    C,
+    Csharp,
+    Javascript,
+    Ruby,
+    Bash,
+    Swift,
+    Golang,
+    Scala,
+    Html,
+    Pythonml,
+    Kotlin,
+    Rust,
+    Php,
+    Typescript,
+    Racket,
+    Erlang,
+    Elixir,
+    Dart,
+    Pythondata,
+    React,
+    Unknown(u32),
+}
+
+impl Language {
+    fn from_id(id: u32) -> Language {
+        match id {
+            0 => Language::Cpp,
+            1 => Language::Java,
+            2 => Language::Python,
+            11 => Language::Python3,
+            3 => Language::Mysql,
+            14 => Language::Mssql,
+            15 => Language::Oraclesql,
+            4 => Language::C,
+            5 => Language::Csharp,
+            6 => Language::Javascript,
+            7 => Language::Ruby,
+            8 => Language::Bash,
+            9 => Language::Swift,
+            10 => Language::Golang,
+            12 => Language::Scala,
+            16 => Language::Html,
+            17 => Language::Pythonml,
+            13 => Language::Kotlin,
+            18 => Language::Rust,
+            19 => Language::Php,
+            20 => Language::Typescript,
+            21 => Language::Racket,
+            22 => Language::Erlang,
+            23 => Language::Elixir,
+            24 => Language::Dart,
+            25 => Language::Pythondata,
+            26 => Language::React,
+            _ => Language::Unknown(id),
+        }
+    }
+
+    fn to_string(&self) -> String {
+        match self {
+            Language::Cpp => "cpp".to_string(),
+            Language::Java => "java".to_string(),
+            Language::Python => "python".to_string(),
+            Language::Python3 => "python3".to_string(),
+            Language::Mysql => "mysql".to_string(),
+            Language::Mssql => "mssql".to_string(),
+            Language::Oraclesql => "oraclesql".to_string(),
+            Language::C => "c".to_string(),
+            Language::Csharp => "csharp".to_string(),
+            Language::Javascript => "javascript".to_string(),
+            Language::Ruby => "ruby".to_string(),
+            Language::Bash => "bash".to_string(),
+            Language::Swift => "swift".to_string(),
+            Language::Golang => "golang".to_string(),
+            Language::Scala => "scala".to_string(),
+            Language::Html => "html".to_string(),
+            Language::Pythonml => "pythonml".to_string(),
+            Language::Kotlin => "kotlin".to_string(),
+            Language::Rust => "rust".to_string(),
+            Language::Php => "php".to_string(),
+            Language::Typescript => "typescript".to_string(),
+            Language::Racket => "racket".to_string(),
+            Language::Erlang => "erlang".to_string(),
+            Language::Elixir => "elixir".to_string(),
+            Language::Dart => "dart".to_string(),
+            Language::Pythondata => "pythondata".to_string(),
+            Language::React => "react".to_string(),
+            Language::Unknown(id) => format!("Unknown({})", id),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::collections::HashMap;
+    #[test]
+    fn test() {
+        // JSON data as a string
+        let json_data = r#"
+        {
+            "data": {
+                "languageList": [
+                    { "id": 0, "name": "cpp" },
+                    { "id": 1, "name": "java" },
+                    { "id": 2, "name": "python" },
+                    { "id": 11, "name": "python3" },
+                    { "id": 3, "name": "mysql" },
+                    { "id": 14, "name": "mssql" },
+                    { "id": 15, "name": "oraclesql" },
+                    { "id": 4, "name": "c" },
+                    { "id": 5, "name": "csharp" },
+                    { "id": 6, "name": "javascript" },
+                    { "id": 7, "name": "ruby" },
+                    { "id": 8, "name": "bash" },
+                    { "id": 9, "name": "swift" },
+                    { "id": 10, "name": "golang" },
+                    { "id": 12, "name": "scala" },
+                    { "id": 16, "name": "html" },
+                    { "id": 17, "name": "pythonml" },
+                    { "id": 13, "name": "kotlin" },
+                    { "id": 18, "name": "rust" },
+                    { "id": 19, "name": "php" },
+                    { "id": 20, "name": "typescript" },
+                    { "id": 21, "name": "racket" },
+                    { "id": 22, "name": "erlang" },
+                    { "id": 23, "name": "elixir" },
+                    { "id": 24, "name": "dart" },
+                    { "id": 25, "name": "pythondata" },
+                    { "id": 26, "name": "react" }
+                ]
+            }
+        }
+    "#;
+
+        // Parse JSON data into the Languages struct
+        let languages: Languages = serde_json::from_str(json_data).unwrap();
+
+        // Extract the languageList
+        let language_list = languages.data.language_list;
+
+        // Create a HashMap to store Language enums by id
+        let mut language_map: HashMap<u32, Language> = HashMap::new();
+        for lang_info in language_list {
+            language_map.insert(lang_info.id, Language::from_id(lang_info.id));
+        }
+
+        // Example: Accessing the language by id
+        let id_to_find = 2; // Example: "python"
+        if let Some(lang) = language_map.get(&id_to_find) {
+            println!("Language with id {}: {:?}", id_to_find, lang);
+        }
     }
 }
---
+++ src/graphql/run_code.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+
+use super::{GQLLeetcodeQuery, Language};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RunSolution {
+    lang: Language,
+    question_id: String,
+    typed_code: String,
+    #[serde(rename = "data_input")]
+    test_cases_stdin: String,
+    #[serde(skip_serializing, skip_deserializing)]
+    slug: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TestCase {
+    interpret_id: String,
+    test_cases: String,
+}
+
+impl GQLLeetcodeQuery for RunSolution {
+    type T = TestCase;
+
+    fn get_endpoint(&self) -> String {
+        let slug = self.slug.as_str();
+        format!("https://leetcode.com/problems/{slug}/interpret_solution/")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::RunSolution;
+
+    #[test]
+    fn test() {
+        let s = RunSolution {
+            lang: crate::graphql::Language::Python3,
+            question_id: "1".to_string(),
+            typed_code: "class Solution:\n    def twoSum(self, nums: List[int], target: int) -> List[int]:    return [4]".to_string(),
+            test_cases_stdin: "[2,7,11,15]\n9\n[3,2,4]\n6\n[3,3]\n6".to_string(),
+            slug: "".to_string(),
+        };
+
+        let from_struct = json!(s);
+        let from_raw_json = json!(
+            {
+             "lang": "python3",
+             "question_id": "1",
+             "typed_code": "class Solution:\n    def twoSum(self, nums: List[int], target: int) -> List[int]:    return [4]",
+             "data_input": "[2,7,11,15]\n9\n[3,2,4]\n6\n[3,3]\n6"
+            }
+        );
+        assert_eq!(from_struct, from_raw_json);
+    }
+}
