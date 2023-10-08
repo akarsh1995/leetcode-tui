e5bb18e18f074a961a1a2ad8f93a19b5fce30743
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-10-08 11:12:46 +0530

    refactor: popup use global layout

--- app/src/popup.rs
+++ app/src/popup.rs
@@ -1,10 +1,9 @@
-// use crossterm::terminal::Clear;
+use crate::ctx::Ctx;
 use ratatui::prelude::*;
 use ratatui::widgets::{
     Block, Borders, Clear, List, ListItem, Paragraph, Scrollbar, StatefulWidget, Widget, Wrap,
 };
-
-use crate::ctx::Ctx;
+use shared::layout::GetWindowStats;
 
 pub struct SelectPopup<'a> {
     ctx: &'a mut Ctx,
@@ -17,20 +16,18 @@ impl<'a> SelectPopup<'a> {
 }
 
 impl<'a> Widget for SelectPopup<'a> {
-    fn render(self, area: Rect, buf: &mut Buffer) {
+    fn render(self, _area: Rect, buf: &mut Buffer) {
         let mut block: Block<'_> = Block::default();
         if let Some(title) = self.ctx.select_popup.get_title() {
             block = Block::default().title(title);
         }
         let block = block.borders(Borders::ALL);
-        let area = centered_rect(60, 60, area);
-        Clear.render(area, buf);
-        let inner = block.inner(area);
-        block.render(area, buf);
+        Clear.render(self.get_window().root.popup.outer, buf);
+        block.render(self.get_window().root.popup.outer, buf);
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints(vec![Constraint::Percentage(100), Constraint::Min(1)])
-            .split(inner);
+            .split(self.get_window().root.popup.inner);
         let content_area = chunks[0];
         let lines = self.ctx.select_popup.get_lines().clone();
 
@@ -79,20 +76,18 @@ impl<'a> Popup<'a> {
 }
 
 impl<'a> Widget for Popup<'a> {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
+    fn render(self, _area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
         let mut block: Block<'_> = Block::default();
         if let Some(title) = self.ctx.popup.get_title() {
             block = Block::default().title(title);
         }
         let block = block.borders(Borders::ALL);
-        let area = centered_rect(60, 60, area);
-        Clear.render(area, buf);
-        let inner = block.inner(area);
-        block.render(area, buf);
+        Clear.render(self.get_window().root.popup.outer, buf);
+        block.render(self.get_window().root.popup.outer, buf);
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints(vec![Constraint::Percentage(100), Constraint::Min(1)])
-            .split(inner);
+            .split(self.get_window().root.popup.inner);
         let content_area = chunks[0];
         let scrollbar_area = chunks[1];
         self.prepare_paragraph().render(content_area, buf);
@@ -102,31 +97,4 @@ impl<'a> Widget for Popup<'a> {
             .end_symbol(Some("↓"))
             .render(scrollbar_area, buf, &mut self.ctx.popup.v_scroll_state)
     }
-}
-
-/// helper function to create a centered rect using up certain percentage of the available rect `r`
-fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_y) / 2),
-                Constraint::Percentage(percent_y),
-                Constraint::Percentage((100 - percent_y) / 2),
-            ]
-            .as_ref(),
-        )
-        .split(r);
-
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_x) / 2),
-                Constraint::Percentage(percent_x),
-                Constraint::Percentage((100 - percent_x) / 2),
-            ]
-            .as_ref(),
-        )
-        .split(popup_layout[1])[1]
 }
